a62948820d655d00bdf4a2065e60c4fd18ce1f81
dee.std <mxllywd@gmail.com>
2024-09-12 21:26:32 +0300

    Updated PasswordManager.cs

--- PasswordManager.cs
+++ PasswordManager.cs
@@ -4,5 +4,31 @@ using BCrypt.Net;
 
 namespace password_manager
 {
-  //some code//
+  public class PasswordManager
+  {
+    private readonly string connection;
+
+    public PasswordManager(string dbPath)
+    {
+      connection = $"Data source ={dbPath}, version = 3;";
+      InitializeDatabase();
+    }
+
+    private void InitializeDatabase()
+    {
+      using (var connect = new SQLiteConnection(connection))
+      {
+        connections.Open()
+        string createTableQuery = @"CREATE TABLE IF NOT EXISTS Passwords (
+        Id INTEGER PRIMARY KEY AUTOINCREMENT,
+        Login TEXT NOT NULL UNIQUE,
+        PasswordHash TEXT NOT NULL)";
+        using (var command = new SQLiteCommand(createTableQuery, connection))
+        {
+          command.ExecuteNonQuery();
+        }
+      }
+    }
+    // some code
+  }
 }

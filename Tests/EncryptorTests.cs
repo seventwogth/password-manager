43e5e2a1425409490d0e67f18c4c6eed228b33c5
deetherockstar <163294265+deetherockstar@users.noreply.github.com>
2024-10-14 18:36:14 +0300

    Add Encryption tests

--- Tests/EncryptorTests.cs
+++ Tests/EncryptorTests.cs
@@ -1,4 +1,61 @@
 using NUnit;
-using Pmanager.Cryptography;
+using NUnit.Framework;
+using PManager.Cryptography;
 
+namespace PManager.Tests
+{
+  [TestFixture]
+  public class EncryptorTests
+  {
+
+    private Encryptor _encryptor;
+
+    [SetUp]
+    public void Setup()
+    {
+      _encryptor = new Encryptor();
+    }
+
+    [Test]
+    public void Encryptor_ShouldReturnEncryptedText()
+    {
+      //Arrange
+      var plainText = "password";
+
+      //Act
+      var encryptedText = _encryptor.Encrypt(plainText);
+
+      //Assert
+      Assert.That(plainText, Is.Not.EqualTo(encryptedText));
+    }
+    
+    [Test]
+    public void Encryptor_ShouldReturnDecryptedText()
+    {
+      //Arrange
+      var plainText = "password";
+
+      //Act
+      var encryptedText = _encryptor.Encrypt(plainText);
+      var decryptedText = _encryptor.Decrypt(encryptedText);
+
+      //Assert
+      Assert.That(plainText, Is.EqualTo(decryptedText));
+    }
+
+    [Test]
+    public void Encryptor_ShouldReturnDifferentEncrypted()
+    {
+      //Arrange
+      var plainText = "password";
+
+      //Act
+      var encryptedText1 = _encryptor.Encrypt(plainText);
+      var encryptedText2 = _encryptor.Encrypt(plainText);
+
+      //Assert
+      Assert.That(encryptedText1, Is.Not.EqualTo(encryptedText2));
+    }
+  }
+}
 

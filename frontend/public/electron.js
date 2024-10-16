77644aa2a46dd3766abbd6c5ee3be11a265b968e
deetherockstar <163294265+deetherockstar@users.noreply.github.com>
2024-10-16 20:58:40 +0300

    Added React template

---
+++ frontend/public/electron.js
@@ -0,0 +1,38 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    }
+  });
+
+  mainWindow.loadURL(
+    process.env.ELECTRON_START_URL || `file://${path.join(__dirname, '../build/index.html')}`
+  );
+
+  mainWindow.on('closed', function () {
+    mainWindow = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
+
